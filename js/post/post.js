19016b7c7429fcbb9aebaf12a10c5015187d122d
Malin Skrettingland <malinersdal@hotmail.com>
2025-11-08 22:00:19 +0100

    Refactor post.js to improve structure

--- js/post/post.js
+++ js/post/post.js
@@ -1,26 +1,25 @@
 import { getPostById } from "../api/postsService.js";
 import { isLoggedIn } from "../api/authService.js";
 
-// Get the query string from the current URL
-const urlParams = new URLSearchParams(window.location.search);
 
-// Get the value of a specific parameter, e.g., "id"
-const postId = urlParams.get("id");
 
-const postContainer = document.getElementById('post-container');
 
 function displayPost(post) {
+    const postContainer = document.getElementById('post-container');
+
     const mediaUrl = post.media && post.media.url ? post.media.url : '/image/food_vibe02.jpg';
     const mediaAlt = post.media && post.media.alt ? post.media.alt : 'Generic fallback image';
     const authorName = post.author?.name ?? "Unknown";
     const authorAvatar = post.author?.avatar?.url ? post.author.avatar.url : '/image/avatar.jpg';
 
     postContainer.innerHTML = `
-    <img class="col-md-6 order-md-2 ps-4 d-flex"
+    <img class="col-md-6 order-md-2 d-flex"
     src="${mediaUrl}" alt="${mediaAlt}">
-    <div class="col-md-6 order-md-1">	
-        <h2>${post.title}</h2>
-        <p class="lc-block text-secondary pb-4 post-body"></p>
+    <div class="col-md-6 order-md-1">
+        <div class="p-3">
+            <h2>${post.title}</h2>
+            <p class="lc-block text-secondary pb-4 post-body"></p>
+        </div>
         <div class="author-info d-flex align-items-center p-3 border-top bg-light shadow-sm">
             <img src="${authorAvatar}" alt="${authorName}" class="rounded-circle me-3" style="width:48px; height:48px;"/> 
             <a href="/account/viewProfile.html?u=${authorName}"><strong class="fw-bold">${authorName}</strong></a>
@@ -37,6 +36,12 @@ async function main() {
         return;
     }
     try {
+        // Get the query string from the current URL
+        const urlParams = new URLSearchParams(window.location.search);
+
+        // Get the value of a specific parameter, e.g., "id"
+        const postId = urlParams.get("id");
+
         const post = await getPostById(postId,true);
         displayPost(post);    
     } catch (error) {
